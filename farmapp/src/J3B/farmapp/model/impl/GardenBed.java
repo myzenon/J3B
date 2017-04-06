a6a81d29b0d7c5cdb5c7adae7f48ad9da40a9acf
shadowangle <shadowangle1222@hotmail.com>
2017-04-06 22:25:08 +0700

    Implement GardenBed
    -Create a GardenBed class to store Plants
    Need a list of "Plant" and number of capacity of the GardenBed.
    -Create methods to get(),set() Plant from the list and the capacity of
    the GardenBed.
    
    GH-3 GH-4

---
+++ farmapp/src/J3B/farmapp/model/impl/GardenBed.java
@@ -0,0 +1,34 @@
+package J3B.farmapp.model.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GardenBed {
+
+    private List plants = new ArrayList<Plant>();
+    private int capacity;
+    public GardenBed(int capacity){
+    	this.capacity = capacity;
+    }
+    public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public void setPlants(List plants) {
+		this.plants = plants;
+	}
+	public List<Plant> getPlants() {
+        return plants;
+    }
+
+    public void addPlant(Plant plant) {
+    	plants.add(plant);
+    }
+
+    public void removePlant(int position) {
+    	plants.remove(position);
+    }
+
+}
