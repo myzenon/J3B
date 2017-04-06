c1edd26c69fe58d8f962bcb14ec5c00d16c462ce
shadowangle <shadowangle1222@hotmail.com>
2017-04-06 22:24:11 +0700

    Implement Seed and interface "Item"
    -Create Seed class, containing the name of the seed and a plant object.
    -Make Seed implement Item interface.
    -Create methods to get(),set() the name and the plant.
    -Create an item interface so that fruit can also implement it later on
    
    GH-4

---
+++ farmapp/src/J3B/farmapp/model/impl/Seed.java
@@ -0,0 +1,25 @@
+package J3B.farmapp.model.impl;
+
+import J3B.farmapp.model.Item;
+import J3B.farmapp.model.Plantable;
+
+public class Seed implements Plantable,Item{
+	private String name;
+	private Plant plant;
+	public Seed(String name,Plant plant){
+		this.name = name;
+		this.plant = plant;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void plant(){
+		
+	}
+	public Plant getPlant(){
+		return plant;
+	}
+}
