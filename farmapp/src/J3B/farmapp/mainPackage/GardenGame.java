ab64b717c3716c9c2ffec0feba7df36ca1788e07
jetsirawut <onpraphai.sira@gmail.com>
2017-04-07 00:33:50 +0700

    Re-arrange files

--- farmapp/src/J3B/farmapp/mainPackage/GardenGame.java
+++ farmapp/src/J3B/farmapp/mainPackage/GardenGame.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package J3B.farmapp.model.impl;
+package J3B.farmapp.mainPackage;
+
+import J3B.farmapp.controller.GameController;
 
 /**
  *
