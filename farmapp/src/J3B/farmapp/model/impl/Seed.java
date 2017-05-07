17231d2e6b80a2fde6f42373bb7940bfa3cf345a
bewblaze01 <bewblaze01@yahoo.com>
2017-05-08 01:12:32 +0700

    Update Seed Class
    Task : GH-86

--- farmapp/src/J3B/farmapp/model/impl/Seed.java
+++ farmapp/src/J3B/farmapp/model/impl/Seed.java
@@ -12,14 +12,14 @@ public class Seed implements Plantable, Item {
 	public Seed(String name, Plant plant){
 		this.name = name;
 		this.plant = plant;
-	}
+	}	
 
 	public String getName() {
 		return this.name;
 	}
 
 	public Growable plant(){
-		return this.plant;
+		return this.plant.clone();
 	}
 
 }
